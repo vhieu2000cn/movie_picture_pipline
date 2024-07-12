5f63fb76b387fe47634f5067cc82b2c500a650f7
vhieu200cn <vhieu2000cn@gmail.com>
2024-07-12 11:42:44 +0700

    build movie picture application

---
+++ setup/init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e -o pipefail
+
+echo "Fetching IAM github-action-user ARN"
+userarn=$(aws iam get-user --user-name github-action-user | jq -r .User.Arn)
+
+# Download tool for manipulating aws-auth
+echo "Downloading tool..."
+curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/aws-iam-authenticator
+chmod +x aws-iam-authenticator
+
+echo "Updating permissions"
+aws-iam-authenticator add user --userarn="${userarn}" --username=github-action-role --groups=system:masters --kubeconfig="$HOME"/.kube/config --prompt=false
+
+echo "Cleaning up"
+rm aws-iam-authenticator
+echo "Done"
